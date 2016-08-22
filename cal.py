9ec0b2be7ef9ca9b304910bfe93ed21cafb43b63
AFresnedo <AFresnedo@users.noreply.github.com>
2016-08-22 15:17:16 -0700

    Coding tree nodes for cal.py

--- cal.py
+++ cal.py
@@ -16,11 +16,12 @@ class Cal:
     def append(self, time = -1.00):
         None #add information to an appointment to allow for double+ booking or fixing
 
+#TODO i'm thinking appTreeNode and appTreeMultiNode
 class appTree:
     "Tree beginning with a year root, holding month nodes, holding day nodes, holding Day UDT."
     def __init__(self, year = 2016):
         self.year = 2016
-        self.root = appTreeNode('year', year)
+        self.root = appTreeDay('year', year)
     
     #post: return reference to appropriate Day UDT
     def getDay(self, date):
@@ -40,44 +41,37 @@ class appTree:
         #   appointments scheduled?" stuff like checking strucutre should not be done here
 
 class appTreeNode:
-    "A node in appTree holding the year, month, xor day."
-    #pre: parent (year or month) node and date to hold
-    #post: creates node holding date and linked to parent
-    def __init__(self, ident = '', date = -1, parent = None):
-        self.ident = ident #TODO asserts to only allow 'year', 'month', xor 'day'
-        self.date = date #TODO asserts for valid number
-        self.parent = parent #TODO asserts to properly structure
-        self.children = None
+    "A day in appTree holding its date and its links."
+    def __init__(self, parent = None, date = -1, child = None):
+        self.parent = parent
+        self.date = date
+        self.child = day.Day()
     
     #TODO: determine if pass by reference is what is wanted here...depends if I want client to have
     #   access to interworking of the tree from just getting a parent of a node, make this class
     #   private? that avoids issues since it's okay for appTree to manipulate nodes
-    #pre: self.node not a year
-    #post: returns parent
+    #post: returns ref to month node
     def getParent(self):
-        assert self.ident is not 'year' #TODO make sure this is proper, instead of !=
-        return self.parent
+        return self.month
+
+    def getSelf(self):
+        return self.day
     
-    #post: returns child if exists, otherwise returns None
     def getChild(self):
         return self.child
 
-    #pre: a month node if self.node is a year, day node if self.node is a month, otherwise Day UDT
-    #post: sets arg as child for self.node
-    def addChild(self, child = None):
-        assert child is not None
-        if self.ident is 'year':
-            assert child.ident is 'month'
-        if self.ident is 'month':
-            assert child.ident is 'day'
-        if self.ident is 'day':
-            #assert child.ident is a Day #TODO 
-            None
-        for c in self.children:
-            if child == c:
-                None #TODO will break get me out of the for loop? that would be nice
-
-
+class appTreeMultiNode(appTreeNode):
+    "The root, a year, or a month in appTree holding their dates and links."
+    def __init__(self, parent = None, date = -1, child = None):
+        None
+    def getDay(self, day = -1):
+        #TODO asserts, nicer if/else and return
+        for d in self.dayL:
+            if d.getDay is day:
+                return d
+            else:
+                return -1
+    
 #hash based on unique year/month/day...and starting hour?
 class appTable:
     def __init__(self):
